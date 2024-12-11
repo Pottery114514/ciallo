81ef8e3d1769542a5fcf10fe6946eeaf2996781a
yoshino <96348436+Pottery114514@users.noreply.github.com>
2024-12-11 22:35:06 +0800

    Update Time.cpp
    优化掉了繁杂的嵌套分支,将思路转变为先统一以秒表示,最后再转换为24小时制.输出加入了前补0.

--- Algorithm/Time.cpp
+++ Algorithm/Time.cpp
@@ -1,37 +1,26 @@
-//保证pt<=100000
 #include <bits/stdc++.h>
 using namespace std;
 struct Time{
     int h,m,s;
 };
-//感觉特别臃肿。
-Time add(Time T,int pt){
-        if (T.s+pt<60){
-            T.s+=pt;
-        }else{//s进位
-            if ((T.s+pt)/60+T.m<60){
-                T.m+=(T.s+pt)/60;
-                T.s=(T.s+pt)%60;
-            }else{//m进位
-                if (T.h+(pt/3600)<24){
-                    T.h+=pt/3600;
-                    T.m+=(pt%3600)/60;
-                    T.s+=(pt%60);
-                }//else{//h进位}
-            }
-        }
-        return T;
-    }
+int toSecond(Time nT){
+    return nT.h*3600+nT.m*60+nT.s;
+}
+Time form(int sum){
+    Time ans;
+    ans.h=(sum/3600)%24;
+    ans.m=(sum%3600)/60;
+    ans.s=sum%60;
+    return ans;
+}
 int main()
 {
     int pt;
     Time nowTime;
-    //cin>>nowTime.h>>nowTime.m>>nowTime.s;
     scanf("%d:%d:%d",&nowTime.h,&nowTime.m,&nowTime.s);
     cin>>pt;
-    
-    Time ans=add(nowTime,pt);
-    printf("%d:%d:%d",ans.h,ans.m,ans.s);//未设置前补0
-
+    int nowTime_s=toSecond(nowTime);
+    Time result=form(nowTime_s+pt);
+    printf("%02d:%02d:%02d",result.h,result.m,result.s);
     return 0;
 }
